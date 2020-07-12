8950da17626e58ad8c4b14db0b292ac1c3dccbce
ijgnd <fooijgnd@barijgnd>
2020-07-12 20:58:22 +0200

    add jquery 351

--- src/external_js_editor_for_field.py
+++ src/external_js_editor_for_field.py
@@ -49,10 +49,29 @@ This add-on bundles the ckeditor theme "moono-dark" in  web/ckeditor4/skins/moon
     Copyright and License are the same as ckedtior4 according to web/ckeditor4/skins/moono-dark/readme.md
 
 
-This add-on bundles jquery3.5.1 in the folder web/ckeditor4
-    Copyright: JS Foundation and other contributors 
-    License: jquery.org/license (MIT)
+This add-on bundles "jquery-3.5.1.min.js" in the folder web which has this copyright and permission
+notice (jquery.org/license, https://github.com/jquery/jquery/blob/master/LICENSE.txt):
 
+    Copyright OpenJS Foundation and other contributors, https://openjsf.org/
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 This add-on bundles "CKEditor 5 classic editor build" (version 19.1.1) in the folder web/ckeditor5
